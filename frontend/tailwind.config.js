8511da5888ff1b8151ffb2d57acf1521338f9aa3
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2022-04-19 03:25:41 +0100

    index page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./**/*.{html,js}", "./html_files/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      heading: ['Poppins', 'sans-serif']
+    }
   },
   plugins: [],
 };
