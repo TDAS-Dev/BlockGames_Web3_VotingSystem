0b4c01e8733ecac3d438f31fccdbe6e940b9fd39
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2022-04-22 20:18:27 +0100

    commit

--- frontend/js_files/vote.js
+++ frontend/js_files/vote.js
@@ -405,32 +405,32 @@ async function displayCandidatesOnScreen() {
         <div class="cursor-pointer rounded-sm">
         <img src="https://img.freepik.com/free-photo/3d-rendering-male-character-profile-with-cream-hat-orange-polo-shirt-good-character-profile_477250-61.jpg?size=338&ext=jpg&ga=GA1.1.1906834557.1622206067" alt="voters image" class="h-52 w-52">
         <div class="bg-zinc-700 text-center w-52">
-        <p class="font-sans font-light">${candidatesArray[0][1]}</p>
         <h1 class="text-lg font-bold">ID: ${candidatesArray[0][0]}</h1>
+        <p class="font-sans font-light">${candidatesArray[0][1]}</p>
         </div>
         </div>
 
         <div class="cursor-pointer rounded-sm">
           <img src="https://img.freepik.com/free-photo/3d-rendering-male-character-profile-with-cream-hat-orange-polo-shirt-good-character-profile_477250-61.jpg?size=338&ext=jpg&ga=GA1.1.1906834557.1622206067" alt="voters image" class="h-52 w-52">
           <div class="bg-zinc-700 text-center w-52">
-          <p class="font-sans font-light">${candidatesArray[1][1]}</p>
           <h1 class="text-lg font-bold">ID: ${candidatesArray[1][0]}</h1>
+          <p class="font-sans font-light">${candidatesArray[1][1]}</p>
           </div>
         </div>
 
         <div class="cursor-pointer rounded-sm">
           <img src="https://img.freepik.com/free-photo/3d-rendering-male-character-profile-with-cream-hat-orange-polo-shirt-good-character-profile_477250-61.jpg?size=338&ext=jpg&ga=GA1.1.1906834557.1622206067" alt="voters image" class="h-52 w-52">
           <div class="bg-zinc-700 text-center w-52">
-          <p class="font-sans font-light">${candidatesArray[2][0]}</p>
-          <h1 class="text-lg font-bold">ID: ${candidatesArray[2][1]}</h1>
+          <h1 class="text-lg font-bold">ID: ${candidatesArray[2][0]}</h1>
+          <p class="font-sans font-light">${candidatesArray[2][1]}</p>
           </div>
         </div>
 
         <div class="cursor-pointer rounded-sm">
           <img src="https://img.freepik.com/free-photo/3d-rendering-male-character-profile-with-cream-hat-orange-polo-shirt-good-character-profile_477250-61.jpg?size=338&ext=jpg&ga=GA1.1.1906834557.1622206067" alt="voters image" class="h-52 w-52">
           <div class="bg-zinc-700 text-center w-52">
-          <p class="font-sans font-light">${candidatesArray[3][0]}</p>
-          <h1 class="text-lg font-bold">ID: ${candidatesArray[3][1]}</h1>
+          <h1 class="text-lg font-bold">ID: ${candidatesArray[3][0]}</h1>
+          <p class="font-sans font-light">${candidatesArray[3][1]}</p>
           </div>
         </div>
   `;
